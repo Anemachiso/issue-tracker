c97ca4939eb3a74a05ebd0a1aacad167cab17e72
Mulugeta W <anem1351@mail.com>
2025-11-21 14:48:12 +0300

    apply Spinner on Button

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -11,6 +11,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { createIssueSchema } from '@/app/validationSchemas';
 import z from 'zod';
 import ErrorMessage from '@/app/components/ErrorMessage';
+import Spinner from '@/app/components/Spinner';
 
 
 const SimpleMdeReact = dynamic(() => import("react-simplemde-editor"), {
@@ -22,6 +23,7 @@ type IssueForm = z.infer<typeof createIssueSchema>;
 const NewIssuePage = () => {
 
   const [error, setError] = useState('');
+  const [isSubmitting, setSubmitting] = useState(false);
 
   const router = useRouter();
   const {register, control, handleSubmit, formState: { errors }} = useForm<IssueForm>({
@@ -30,9 +32,11 @@ const NewIssuePage = () => {
 
   const onSubmit = async (data: IssueForm) => {
     try {
+      setSubmitting(true);
       await axios.post("/api/issues", data);
       router.push("/issues");
     } catch (error) {
+      setSubmitting(false);
       setError('An unexpected error occured!!')
     }
   };
@@ -60,7 +64,7 @@ const NewIssuePage = () => {
           <ErrorMessage>
             {errors.description?.message}
           </ErrorMessage>
-          <Button>Submit New Issue</Button>
+          <Button disabled={isSubmitting}>Submit New Issue {isSubmitting && <Spinner />}</Button>
       </form>
     </div>
   )
