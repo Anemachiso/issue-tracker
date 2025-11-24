a38aa0e400bd1b8c77ce9f55a99a8464cb8359e3
Mulugeta W <anem1351@mail.com>
2025-11-24 14:18:52 +0300

    add additional loading skeleton

--- app/components/Link.tsx
+++ app/components/Link.tsx
@@ -1,3 +1,4 @@
+
 import NextLink from 'next/link';
 import { Link as RadixLink } from '@radix-ui/themes';
 
@@ -14,4 +15,4 @@ const Link = ({href, children}: Props) => {
   )
 }
 
-export default Link
+export default Link;
