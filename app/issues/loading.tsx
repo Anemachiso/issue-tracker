53260170dca5958ebb8a8bf678767610ad1f1f1f
Mulugeta W <anem1351@mail.com>
2025-11-24 14:51:02 +0300

    refactoring organizing imports

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,9 +1,5 @@
-import { Button, Table } from '@radix-ui/themes'
-import Link from 'next/link'
-import { prisma } from '@/prisma/client'
-import IssueStatusBadge from '../components/IssueStatusBadge'
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
+import { Table } from '@radix-ui/themes'
+import { Skeleton } from "@/app/components";
 import IssueActions from './IssueActions'
 
 const LoadingIssuesPage = () => {
