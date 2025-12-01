f24dfd749580d2c214d3191c9fa9215c659212d1
Mulugeta W <anem1351@mail.com>
2025-12-01 15:39:49 +0300

    Implement pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { SearchParams } from "next/dist/server/request/search-params";
 import Pagination from "./components/Pagination";
 
-export default function Home() {
+export default async function Home({searchParams}: {searchParams: Promise<{page: string}>}) {
+
+  const params = await searchParams;
+
   return (
-    <Pagination itemCount={100} pageSize={10} currentPage={10}/>
+    <Pagination itemCount={100} pageSize={10} currentPage={parseInt(params.page)}/>
   );
 }
